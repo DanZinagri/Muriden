66579b03d55d9fe2f2d52eb869c2d750ece001f3
DanZinagri <98767002+DanZinagri@users.noreply.github.com>
2025-11-13 14:43:46 -0600

    settings for dirtmole

--- Source/MuradenLibraries/MuridenMod.cs
+++ Source/MuradenLibraries/MuridenMod.cs
@@ -10,10 +10,46 @@ namespace MuridenLibraries;
 public class MuridenMod : Mod
 {
     public static Harmony Harm;
+    public static MuridenSettings Settings;
 
     public MuridenMod(ModContentPack content) : base(content)
     {
         Harm = new("DanZinagri.Muriden");
-        //Settings = GetSettings<MuridenSettings>(); maybe one day
+        Settings = GetSettings<MuridenSettings>();
+
+        LongEventHandler.ExecuteWhenFinished(ApplySettings);
+    }
+
+    public override string SettingsCategory() => "MuridenTitle".Translate();
+
+    public override void WriteSettings()
+    {
+        base.WriteSettings();
+        //ApplySettings();
+    }
+
+    private void ApplySettings()
+    {
+        //HediffDefOf.PsychicAmplifier.maxSeverity = Settings.maxLevel;
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        Listing_Standard listing = new();
+        listing.Begin(inRect);
+        listing.CheckboxLabeled("Muriden.dirtmolePatch".Translate(), ref Settings.dirtmolePatch, "Muriden.dirtmolePatch.Desc".Translate());
+        listing.End();
+    }
+}
+
+public class MuridenSettings : ModSettings
+{
+    public bool dirtmolePatch = true;
+
+    public override void ExposeData()
+    {
+        base.ExposeData();
+        Scribe_Values.Look(ref dirtmolePatch, nameof(dirtmolePatch), true);
     }
 }
---
+++ Source/MuradenLibraries/PatchOperationEnabled.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Verse;
+
+namespace MuridenLibraries
+{
+    public class PatchOperationEnabled : PatchOperation
+    {
+        // All child operations inside this wrapper
+        public List<PatchOperation> operations;
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            // The variable you already have in settings
+            if (!MuridenMod.Settings.dirtmolePatch)
+            {
+                // Skip all operations when disabled
+                return true;
+            }
+
+            bool result = true;
+
+            // Execute inner operations normally
+            foreach (PatchOperation op in operations)
+            {
+                if (!op.Apply(xml))
+                    result = false;
+            }
+
+            return result;
+        }
+    }
+}
