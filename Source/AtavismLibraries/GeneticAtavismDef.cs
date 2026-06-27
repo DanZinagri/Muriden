31e67286716a21aa1f4fbced6b883b4906979cfd
DanZinagri <98767002+DanZinagri@users.noreply.github.com>
2026-06-27 14:16:35 -0500

    atavism or source xeno can be null now

--- Source/AtavismLibraries/GeneticAtavismDef.cs
+++ Source/AtavismLibraries/GeneticAtavismDef.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
@@ -18,11 +19,21 @@ namespace AtavismLibraries
 
         public bool Matches(Pawn pawn)
         {
-            if (pawn?.genes == null) return false;
-            if (requiredGene == null || sourceXenotype == null || targetXenotype == null) return false;
+            //pawn borked, or you didn't specify a target.
+            if (pawn?.genes == null || targetXenotype == null)
+                return false;
 
-            return pawn.genes.Xenotype == sourceXenotype
-                && AtavismUtils.HasActiveGene(pawn, requiredGene);
+            // Invalid def: no condition was provided.
+            if (requiredGene == null && sourceXenotype == null)
+                return false;
+
+            bool geneMatches = requiredGene == null ||
+                       AtavismUtils.HasActiveGene(pawn, requiredGene);
+
+            bool xenotypeMatches = sourceXenotype == null ||
+                                   pawn.genes.Xenotype == sourceXenotype;
+
+            return geneMatches && xenotypeMatches;
         }
     }
 
