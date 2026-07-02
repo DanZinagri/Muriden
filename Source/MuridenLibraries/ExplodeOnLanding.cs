dafba5e368f4306ff9ffe37a28127886d317b360
DanZinagri <98767002+DanZinagri@users.noreply.github.com>
2026-07-01 22:40:03 -0500

    newxenotype and fixes

--- Source/MuridenLibraries/ExplodeOnLanding.cs
+++ Source/MuridenLibraries/ExplodeOnLanding.cs
@@ -15,7 +15,7 @@ public class CompProperties_ExplodeOnLanding : CompProperties_AbilityEffect
     public float radius = 10f;
     public DamageDef damageDef = DamageDefOf.Bomb;
     public int damageAmount = -1; // use -1 for default by damageDef if you want
-    public SoundDef sound;
+    public SoundDef MLsound;
     public bool casterImmune = true;
     public bool damageFalloff = false;
     public bool radiusOnPawnSize = false;
@@ -34,7 +34,7 @@ public class CompProperties_ExplodeOnLanding : CompProperties_AbilityEffect
 
 public class CompAbilityEffect_ExplodeOnLanding : CompAbilityEffect
 {
-    public CompProperties_ExplodeOnLanding Props => (CompProperties_ExplodeOnLanding)props;
+    public CompProperties_ExplodeOnLanding MLProps => (CompProperties_ExplodeOnLanding)props;
 
     public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
     {
@@ -48,40 +48,40 @@ public class CompAbilityEffect_ExplodeOnLanding : CompAbilityEffect
         if (!landing.InBounds(pawn.Map) || !landing.Walkable(pawn.Map))
             CellFinder.TryFindRandomCellNear(landing, pawn.Map, 2, c => c.Walkable(pawn.Map), out landing);
 
-        float radius = Props.radius * (Props.radiusOnPawnSize ? pawn.BodySize : 1f);
-        int damage = Props.damageAmount * (Props.damageOnPawnSize ? (int)(pawn.BodySize) : 1);
-        Thing instigator = Props.casterImmune ? null : pawn;
+        float radius = MLProps.radius * (MLProps.radiusOnPawnSize ? pawn.BodySize : 1f);
+        int damage = MLProps.damageAmount * (MLProps.damageOnPawnSize ? (int)(pawn.BodySize) : 1);
+        Thing instigator = MLProps.casterImmune ? null : pawn;
 
         GenExplosion.DoExplosion(
             center: landing,
             map: pawn.Map,
             radius: radius,
-            damType: Props.damageDef,
+            damType: MLProps.damageDef,
             instigator: instigator,
             damAmount: damage,
             armorPenetration: -1f,
-            explosionSound: Props.sound,
+            explosionSound: MLProps.sound,
             weapon: null,
             projectile: null,
             intendedTarget: null,
             postExplosionSpawnThingDef: null,
             postExplosionSpawnChance: 0f,
             postExplosionSpawnThingCount: 1,
-            applyDamageToExplosionCellsNeighbors: Props.damageFalloff,
+            applyDamageToExplosionCellsNeighbors: MLProps.damageFalloff,
             chanceToStartFire: 0f,
-            damageFalloff: Props.damageFalloff
+            damageFalloff: MLProps.damageFalloff
         );
 
-        if (Props.shockwaveFleck != null)
+        if (MLProps.shockwaveFleck != null)
         {
-            float visualScale = Props.shockwaveScaleWithRadius
-                ? radius * Props.shockwaveScale
-                : Props.shockwaveScale;
+            float visualScale = MLProps.shockwaveScaleWithRadius
+                ? radius * MLProps.shockwaveScale
+                : MLProps.shockwaveScale;
 
             FleckMaker.Static(
                 landing.ToVector3Shifted(),
                 pawn.Map,
-                Props.shockwaveFleck,
+                MLProps.shockwaveFleck,
                 visualScale
             );
         }
---
+++ Source/MuridenLibraries/GeneAbilities.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using VEF.Abilities;
+using AbilityDef = VEF.Abilities.AbilityDef;
+
+namespace MuridenLibraries
+{
+    public class GeneExtension_VEFAbilities : DefModExtension
+    {
+        public List<AbilityDef> abilityDefs = new();
+    }
+
+    public class Gene_GiveVEFAbilities : Gene
+    {
+        public override void PostAdd()
+        {
+            base.PostAdd();
+
+            GeneExtension_VEFAbilities ext = def.GetModExtension<GeneExtension_VEFAbilities>();
+            if (ext?.abilityDefs == null || pawn == null)
+                return;
+
+            CompAbilities comp = pawn.TryGetComp<CompAbilities>();
+            if (comp == null)
+                return;
+
+            foreach (AbilityDef ability in ext.abilityDefs)
+            {
+                if (ability != null && !comp.HasAbility(ability))
+                {
+                    comp.GiveAbility(ability);
+                }
+            }
+        }
+    }
+}
